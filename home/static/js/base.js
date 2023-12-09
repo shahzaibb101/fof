31a4ac7ae0151a2577ece6bbd03c5c532b31fde6
Ramish Hassan Riaz <ramishscrapper@gmail.com>
2023-12-09 17:53:03 +0500

    Styling of homepage

--- home/static/js/base.js
+++ home/static/js/base.js
@@ -1,35 +1,54 @@
 const btnCollapse = document.getElementById('btn-collapse');
-        const topBar = document.getElementById('top-bar');
-        const middleBar = document.getElementById('middle-bar');
-        const bottomBar = document.getElementById('bottom-bar');
-      
-        let isWhite = false;
-      
-        btnCollapse.addEventListener('click', function() {
-          if (isWhite) {
-            topBar.style.position = 'absolute';
-            topBar.style.top = '0px';
-            topBar.style.transform = 'rotate(0deg)';
+const topBar = document.getElementById('top-bar');
+const middleBar = document.getElementById('middle-bar');
+const bottomBar = document.getElementById('bottom-bar');
 
-            middleBar.style.opacity = '1';
-            middleBar.style.position = 'absolute';
-            middleBar.style.top = '10px';
-            middleBar.style.filter = 'alpha(opacity=100)';
+let isWhite = false;
 
-            bottomBar.style.position = 'absolute';
-            bottomBar.style.top = '20px';
-            bottomBar.style.transform = 'rotate(0deg)';
+btnCollapse.addEventListener('click', function () {
+    if (isWhite) {
+        topBar.style.position = 'absolute';
+        topBar.style.top = '0px';
+        topBar.style.transform = 'rotate(0deg)';
 
-        } else {
-            topBar.style.top = 'inherit';
-            topBar.style.transform = 'rotate(135deg)';
+        middleBar.style.opacity = '1';
+        middleBar.style.position = 'absolute';
+        middleBar.style.top = '10px';
+        middleBar.style.filter = 'alpha(opacity=100)';
 
-            middleBar.style.opacity = '0';
-            middleBar.style.top = 'inherit';
-            middleBar.style.filter = 'alpha(opacity=0)';
+        bottomBar.style.position = 'absolute';
+        bottomBar.style.top = '20px';
+        bottomBar.style.transform = 'rotate(0deg)';
 
-            bottomBar.style.top = 'inherit';
-            bottomBar.style.transform = 'rotate(-135deg)';
-          }
-          isWhite = !isWhite;
+    } else {
+        topBar.style.top = 'inherit';
+        topBar.style.transform = 'rotate(135deg)';
+
+        middleBar.style.opacity = '0';
+        middleBar.style.top = 'inherit';
+        middleBar.style.filter = 'alpha(opacity=0)';
+
+        bottomBar.style.top = 'inherit';
+        bottomBar.style.transform = 'rotate(-135deg)';
+    }
+    isWhite = !isWhite;
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const accordionTabs = document.querySelectorAll('.accordion-tab');
+
+    accordionTabs.forEach(tab => {
+        tab.addEventListener('click', function () {
+            const content = this.nextElementSibling;
+
+            this.parentElement.classList.toggle('active');
+            this.classList.toggle('active');
+
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
         });
+    });
+});
