7c45e3aea2b08b4cf76e4e74925bd30111d7ef34
Jahanzaib-baig01 <118760237+Jahanzaib-baig01@users.noreply.github.com>
2024-03-18 00:02:24 +0500

    Update settings.py

--- fof/settings.py
+++ fof/settings.py
@@ -116,14 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIR = [
-    BASE_DIR, "static",
-    
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
+
